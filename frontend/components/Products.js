56177a9f19288336f14ea32f4b9d941af54229ab
Dillon Ellis <dillonzellis@gmail.com>
2022-07-04 11:06:20 -0400

    add styled components

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,14 +1,16 @@
+import { ProductStyle } from "../styles/ProductStyle";
+
 export default function Product({ product }) {
   // Extract the info from props
   const { title, price, image } = product.attributes;
 
   return (
-    <div>
+    <ProductStyle>
       <div>
         <img src={image.data.attributes.formats.small.url} alt="" />
       </div>
       <h2>{title}</h2>
       <h3>{price}</h3>
-    </div>
+    </ProductStyle>
   );
 }
